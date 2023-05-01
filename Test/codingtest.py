b232a297f6709aa5b62a377b040edddeb01a5131
MSNDgit <powerover87@gmail.com>
2023-05-01 22:54:45 +0900

    0501-1

---
+++ Test/codingtest.py
@@ -0,0 +1,5 @@
+n = int(input())
+s,y = 1,1
+plans = input().split()
+dx = [0,0,-1,1]
+dy = 
---
+++ basicpython/python.py
@@ -0,0 +1,17 @@
+money = 2000
+card = 1
+money = 2000
+card = 1
+if False | False:
+    print("택시를 타고가라")
+else:
+    print("걸어가라")
+
+
+
+treeHit = 0
+while treeHit <10:
+    treeHit = treeHit +1
+    print("나무를 %d 번 찍었습니다." % treeHit)
+    if treeHit == 10:
+        print("나무 넘어갑니다.")
